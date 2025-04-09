26946b028fde5156afc5ccaf030da96e160cec21
Steve Thomas <stevethomasproject@gmail.com>
2025-04-09 00:10:42 -0400

    more insert to daTABASE

--- database.py
+++ database.py
@@ -20,26 +20,31 @@ def insert_player(data):
   print(data)
   mycursor = mydb.cursor()
 
+  if data["turn_4"][0] == "":
+    data['turn_4'][0] = None
+    data['turn_4'][1] = None
+    data['turn_4'][2] = None
+
   sql = "INSERT INTO players (playerName,playerCity,ticketPurchasedStore,ticketPurchasedStreet, ticketPurchasedCity, number1,number1SpecialEvent, number1score, number2,number2SpecialEvent, number2score, number3,number3SpecialEvent, number3score, number4,number4SpecialEvent, number4score, bonus, isCashChallenge, isSecondChance, isChampion) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
-  datum = (data.player_info[0],
-           data.player_info[1],
-           data.player_info[2],
-           data.player_info[3],
-           data.player_info[4],
-           data.turn_1[0],
-           data.turn_1[1],
-           data.turn_1[2],
-           data.turn_2[0],
-           data.turn_2[1],
-           data.turn_2[2],
-           data.turn_3[0],
-           data.turn_3[1],
-           data.turn_3[2],
-           data.turn_4[0],
-           data.turn_4[1],
-           data.turn_4[2],
-           data.bonus,
+  datum = (data["player_info"][0],
+           data["player_info"][1],
+           data["player_info"][2],
+           data["player_info"][3],
+           data["player_info"][4],
+           data["turn_1"][0],
+           data["turn_1"][1],
+           data["turn_1"][2],
+           data["turn_2"][0],
+           data["turn_2"][1],
+           data["turn_2"][2],
+           data["turn_3"][0],
+           data["turn_3"][1],
+           data["turn_3"][2],
+           data["turn_4"][0],
+           data["turn_4"][1],
+           data["turn_4"][2],
+           data["bonus"],
            False,
            False,
            False)
@@ -48,5 +53,5 @@ def insert_player(data):
 
   mydb.commit()
 
-  mycursor.close()
-  mydb.close()
+  #mycursor.close()
+  #mydb.close()

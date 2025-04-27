eb1dcfb13679530d5ff09f0af62c04d5b669f90b
Steve Thomas <stevethomasproject@gmail.com>
2025-04-27 10:18:55 -0400

    A list of businesses generated from nominatum.  Updated cities from 4-26 episode

--- findaddress.py
+++ findaddress.py
@@ -1,52 +1,65 @@
 import requests
 import os
+import csv
 from dotenv import load_dotenv
 
 load_dotenv()
 
 op_apikey = os.getenv("OPENCAGEAPIKEY")
 
+file_to_load = "players2.csv"
+
+
 api_key = op_apikey
-query = "Sunny's Quick-Stop and Deli, lima, oh"
 
-url = "https://api.opencagedata.com/geocode/v1/json"
-params = {
-    'q': query,
-    'key': api_key,
-    #'limit': 1
-}
+def get_latlng(business, city, road):
+    query = f"{business}, {city}, oh"
 
-response = requests.get(url, params=params).json()
+    url = "https://api.opencagedata.com/geocode/v1/json"
+    params = {
+        'q': query,
+        'key': api_key,
+        #'limit': 1
+    }
 
-print(response)
+    response = requests.get(url, params=params).json()
+
+    if response['results']:
+        result = response['results'][0]
+        #print("Formatted:", result['formatted'])
+        #print("Lat:", result['geometry']['lat'])
+        #print("Lng:", result['geometry']['lng'])
+
+        return getaddress(result['geometry']['lat'], result['geometry']['lng'], road)
+    else:
+        print("Not found")
+
+def getaddress(lat, lng, road):
+    url = 'https://api.opencagedata.com/geocode/v1/json'
+    params = {
+        'q': f'{lat},{lng}',
+        'key': api_key,
+        'pretty': 1,
+        'no_annotations': 1
+    }
+
+    response = requests.get(url, params=params).json()
+
+    if response['results']:
+        result = response['results'][0]
+        #print("Formatted address:", result['formatted'])
+        #print(road)
+        #print("Components:", result['components'])  # Optional: access street, city, etc.
+        return([result['formatted'], road, result["components"],lat,lng])
+    else:
+        print("No results found")
 
 
-if response['results']:
-    result = response['results'][0]
-    print("Formatted:", result['formatted'])
-    print("Lat:", result['geometry']['lat'])
-    print("Lng:", result['geometry']['lng'])
-else:
-    print("Not found")
-
-'''
-lat = 40.739306  # Example latitude
-lng = -84.138570  # Example longitude
-
-url = 'https://api.opencagedata.com/geocode/v1/json'
-params = {
-    'q': f'{lat},{lng}',
-    'key': api_key,
-    'pretty': 1,
-    'no_annotations': 1
-}
-
-response = requests.get(url, params=params).json()
-
-if response['results']:
-    result = response['results'][0]
-    print("Formatted address:", result['formatted'])
-    print("Components:", result['components'])  # Optional: access street, city, etc.
-else:
-    print("No results found")
-'''
+with open(file_to_load) as file:
+    reader = csv.reader(file)
+    header = next(reader)
+    with open("businesses.txt", "a+") as f:
+        for row in reader:
+            info = get_latlng(row[4], row[6], row[5])
+            #print(info)
+            f.write(f"{info[0]}\n{info[1]}\n{info[2]}\n{info[3]}\n{info[4]}\n---------------------------------------------------------------------------------------\n")
